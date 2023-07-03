e3c873e8d62ebea7242c7b746fb3a1519b78a96c
Hritu-king <87654408+Hritu-king@users.noreply.github.com>
2023-07-03 11:33:26 +0530

    Update config.py

--- helper/config.py
+++ helper/config.py
@@ -17,7 +17,7 @@ from . import *
 try:
     APP_ID = config("APP_ID", default=29616312 , cast=int)
     API_HASH = config("API_HASH" , "dd1a05ab4c47a5a037cc067cf4bded27")
-    BOT_TOKEN = config("BOT_TOKEN" , "6026867531:AAGjjBA9bTO_6lhWgxUW8IwrSp6wNAUNHYY)
+    BOT_TOKEN = config("BOT_TOKEN" , "6026867531:AAGjjBA9bTO_6lhWgxUW8IwrSp6wNAUNHYY")
     OWNER = config("OWNER_ID", default=5190902724, cast=int)
     LOG = config("LOG_CHANNEL", default=-1001971871116 , cast=int)
 except Exception as e:
